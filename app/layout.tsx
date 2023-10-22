6b71e84a29cdd95d55acebc86be49a91d1bad09c
Mif2006 <114983743+Mif2006@users.noreply.github.com>
2023-10-22 12:43:15 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden `}
       >
-        {/* <StarsCanvas /> */}
+        <StarsCanvas />
         <Navbar />
         {children}
         <Footer />
